afaf64f1932945bf4e6b734817a5310bb036a9c9
Diego Gaona <diego@diegogaona.com>
2015-03-23 17:14:11 -0300

    Final

--- plot1.R
+++ plot1.R
@@ -4,12 +4,12 @@ NEI <- readRDS("summarySCC_PM25.rds")
 ## edatasum <- aggregate(Emissions ~ year,edata, sum)
 edatasum <- aggregate(Emissions ~ year,NEI, sum)
 png("plot1.png") ## initiate png graphic device
-## Plot the bar graph, by kilotons of emmissons
+## Plot the bar graph, by millions of tons of emmissons
 barplot((edatasum$Emissions)/10^6,
       names.arg = edatasum$year, ## Use the year for the x axis thicks
       col = "black", ## Bar Color
       xlab = "Year", ## Label the x axis
-      ylab = "PM2.5 Emissions (kiloton)", ## Label the y axis
+      ylab = "PM2.5 Emissions (millions of tons)", ## Label the y axis
       main = "PM2.5 emission from all sources in USA" ## Title
 )
 dev.off()  ## Close the device (png)
